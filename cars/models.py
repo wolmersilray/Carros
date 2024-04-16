1b8c3423a74710b5917dcdc20bab5e88f16c18c2
wolmersilray <wolks.20@hotmail.com>
2024-04-16 19:18:05 -0300

    novo modelo

--- cars/models.py
+++ cars/models.py
@@ -14,7 +14,9 @@ class Car(models.Model):
     brand= models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')#Ligação do campo para outra tabela Brand
     factory_year= models.IntegerField(blank=True, null=True)
     model_year= models.FloatField(blank=True, null=True)
+    plate= models.CharField(max_length=10, blank=True, null=True)
     value= models.FloatField(blank=True, null=True)
+    photo= models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self):
         return self.model
