b0a48f28ca81a18260bc4968608e2a12b515a71b
wolmersilray <wolks.20@hotmail.com>
2024-04-19 14:26:18 -0300

    URLS/TEMPLATS

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import cars_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', cars_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def cars_view(request):
+    return render(
+        request, 
+        'cars.html', 
+        {'cars': {'model': 'Astra 2.0'}}
+    )
