bc101127f7fda5cfdb38ad02a87203d4c76ee09f
wolmersilray <wolks.20@hotmail.com>
2024-04-26 15:04:20 -0300

    Barra menu

--- cars/views.py
+++ cars/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
+    cars= Car.objects.all().order_by('model')
+    search = request.GET.get('search')
     #print(request.GET.get('search'))  #por este comando conseguimos extrair as buscas que o cliente faz.
-    print(request.GET)  #por este comando conseguimos extrair as buscas que o cliente faz.
+    #print(request.GET)  #por este comando conseguimos extrair as buscas que o cliente faz.
 
-    cars= Car.objects.all()  #Busca todas informações dos carros no BD
+    if search:
+        cars= Car.objects.filter(model__contains=search)  
     
-
     #cars= Car.objects.filter(brand=2)  #Busca os carros pela ID cadastrada no BD"
     #cars= Car.objects.filter(brand__name='Chevrolet')  #Busca todos os carros do modelo "Chevrolet"
     #cars= Car.objects.filter(model='Uno')  #Busca todos o modelo de carro no BD
