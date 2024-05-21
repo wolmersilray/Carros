d70a57cab549c241f3b04fe1135ed16c51b0dbe0
wolmersilray <wolks.20@hotmail.com>
2024-05-20 21:34:46 -0500

    new validations

--- cars/forms.py
+++ cars/forms.py
@@ -1,33 +1,19 @@
 from django import forms
-from cars.models import Brand, Car
+from cars.models import Car
 
-#FORMS SIMPLES
-class CarForm(forms.Form):
-    model= forms.CharField(max_length=200)
-    brand= forms.ModelChoiceField(Brand.objects.all())
-    factory_year= forms.IntegerField()
-    model_year= forms.IntegerField()
-    plate= forms.CharField(max_length=10)
-    value= forms.FloatField()
-    photo= forms.ImageField()
-
-    def save(self):
-        car = Car(
-            model= self.cleaned_data['model'],
-            brand= self.cleaned_data['brand'],
-            factory_year= self.cleaned_data['factory_year'],
-            model_year= self.cleaned_data['model_year'],
-            plate= self.cleaned_data['plate'],
-            value= self.cleaned_data['value'],
-            photo= self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
-
-#Model FORMS AVANÇADO (que substitui todo o forms simples)
 class CarModelForm(forms.ModelForm):
-    class Meta: #reescrever a class
-        model= Car #Tabela do BD
-        fields= '__all__'  #Os campos da tabela Cars(nesta caso TODOS)
+    class Meta: 
+        model= Car 
+        fields= '__all__' 
 
+    def clean_value(self): #Sempre usar o CLEAN_(nome do campo) para criar validações 
+        value= self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do carro dever ser de R$20.000,00')
+        return value
 
+    def clean_factory_year(self):
+        factory_year= self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Não é possível cadastra carros fabricados antes de 1975')
+            return factory_year
