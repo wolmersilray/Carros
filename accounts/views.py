e210256f580075f93a169cf8216a0b5af69176f1
wolmersilray <wolks.20@hotmail.com>
2024-06-04 10:57:19 -0500

    Authenticad_View login/ butons

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
 def register_view(request):
@@ -10,3 +11,21 @@ def register_view(request):
     else:
         user_form= UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
+
+def login_view(request):
+    if request.method =="POST":
+        username= request.POST["username"]
+        password= request.POST["password"]
+        user= authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')
+        else:
+            login_form= AuthenticationForm()
+    else:
+        login_form= AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('cars_list')
--- app/urls.py
+++ app/urls.py
@@ -19,11 +19,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, new_car_view
-from accounts.views import register_view
+from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('cars/', cars_view, name='cars_list'),
     path('new_car/', new_car_view, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
