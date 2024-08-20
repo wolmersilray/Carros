0b9022bc5fea07d1ce7583381828b8739b707437
wolmersilray <wolks.20@hotmail.com>
2024-08-20 14:51:12 -0300

    Deploy AWS

--- app/settings.py
+++ app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carros',
+        'USER': 'postgres',
+        'PASSWORD': '211991',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- openia_api/client.py
+++ openia_api/client.py
@@ -17,8 +17,7 @@ def get_car_ai_bio(brand, model, year):
        generation_config= generation_config
         )
 
-    prompt= ''''Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo de carro.
-                Descreva especificações técnicas desse modelo de carro.
+    prompt= ''''Faça uma descrição de venda para o carro {} {} {}  e com especificações técnicas comapenas 250 caracteres.
             '''
     prompt= prompt.format(brand, model, year)
 
